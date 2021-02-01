0a3c2fb5ec9a42ee8e266335f0f68773313513dd
James DeGurse <jamesdegurse@gmail.com>
2021-01-31 19:48:10 -0500

    Code math and operate functions.
    Added the math functions, and operate() function to select which
    operator to use based on calculator input. Defended divide by 0
    errors. May need to refactor any of these depending on how the
    calculator is implemented.

---
+++ static/js/app.js
@@ -0,0 +1,28 @@
+// MATH FUNCTIONS //
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    if (b === 0) return 'Divide by 0 error!'
+    return a / b;
+}
+
+// CALCULATOR FUNCTIONS //
+
+function operate(a, b, operator) {
+    if (operator === '+') return add(a, b);
+    if (operator === '-') return subtract(a, b);
+    if (operator === '*') return multiply(a, b);
+    if (operator === '/') return divide(a, b);
+    return 'operate() error'
+}
